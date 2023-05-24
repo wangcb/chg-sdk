411d06d73abed151fb4b9ddcad7c8c7e1bc0dcd4
wangcb <wangcb615@163.com>
2023-05-24 14:14:30 +0800

    json转下划线

--- response/invoice_vat.go
+++ response/invoice_vat.go
@@ -1,18 +1,18 @@
 package response
 
 type InvoiceVAT struct {
-	NoteCode       string        `json:"noteCode"`       // 发票代码
-	NoteNo         string        `json:"noteNo"`         // 发票号码
-	CheckCode      string        `json:"checkCode"`      // 校验码
-	BillingDate    string        `json:"billingDate"`    // 开票日期
-	PurchaserName  string        `json:"purchaserName"`  // 购买方名称
-	PurchaserTaxNo string        `json:"purchaserTaxNo"` // 购买方税号
-	SellerName     string        `json:"sellerName"`     // 销售方名称
-	SellerTaxNo    string        `json:"sellerTaxNo"`    // 销售方税号
-	InvoiceType    string        `json:"invoiceType"`    // 发票类型
-	InvoiceItems   []InvoiceItem `json:"invoiceItems"`   // 发票明细列表
-	TotalTaxAmount float64       `json:"totalTaxAmount"` // 税额总额
-	TotalAmount    float64       `json:"totalAmount"`    // 价税合计总额
+	NoteCode       string        `json:"note_code"`        // 发票代码
+	NoteNo         string        `json:"note_no"`          // 发票号码
+	CheckCode      string        `json:"check_code"`       // 校验码
+	BillingDate    string        `json:"billing_date"`     // 开票日期
+	PurchaserName  string        `json:"purchaser_name"`   // 购买方名称
+	PurchaserTaxNo string        `json:"purchaser_tax_no"` // 购买方税号
+	SellerName     string        `json:"seller_name"`      // 销售方名称
+	SellerTaxNo    string        `json:"seller_tax_no"`    // 销售方税号
+	InvoiceType    string        `json:"invoice_type"`     // 发票类型
+	InvoiceItems   []InvoiceItem `json:"invoice_items"`    // 发票明细列表
+	TotalTaxAmount float64       `json:"total_tax_amount"` // 税额总额
+	TotalAmount    float64       `json:"total_amount"`     // 价税合计总额
 	OriginResult   string        `json:"origin_result"`
 }
 
@@ -23,6 +23,6 @@ type InvoiceItem struct {
 	Quantity      float64 `json:"quantity"`      // 商品数量
 	Price         float64 `json:"price"`         // 商品单价
 	Amount        float64 `json:"amount"`        // 商品金额
-	TaxRate       float64 `json:"taxRate"`       // 商品税率
-	TaxAmount     float64 `json:"taxAmount"`     // 商品税额
+	TaxRate       float64 `json:"tax_rate"`      // 商品税率
+	TaxAmount     float64 `json:"tax_amount"`    // 商品税额
 }
