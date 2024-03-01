9964052e0e76b453b26fc52a1d3d8614d635f34a
yintao <gushui@139.com>
2024-03-01 16:28:08 +0800

    修改novaSDK

--- server/common.go
+++ server/common.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/wangcb/chg-sdk/chg"
 	"github.com/wangcb/chg-sdk/http"
 	"github.com/wangcb/chg-sdk/request"
@@ -24,12 +26,24 @@ func (s *Common) Request(url string, params map[string]any, method string) (res 
 	return
 }
 
-func (s *Common) NovaRequest(url string, params map[string]any, method string) (res any, err error) {
+func (s *Common) NovaRequest(url string, params map[string]any, method string) (data map[string]interface{}, err error) {
 	req := http.Request{
 		Method: method,
 		URL:    url,
 		Body:   params,
 	}
-	res, err = request.Do(req, chg.Configure.NovaUrl)
-	return
+	res, err := request.Do(req, chg.Configure.NovaUrl)
+	if err != nil {
+		return nil, err
+	}
+	if res.Code != 200 {
+		return nil, errors.New(res.Msg)
+	}
+
+	bytes, _ := json.Marshal(res.Data)
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
