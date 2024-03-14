889f5be676dbb11555c5ac6cf44a93f102a2aa11
yintao <gushui@139.com>
2024-03-14 17:12:04 +0800

    cloud sdk修改

--- server/common.go
+++ server/common.go
@@ -48,12 +48,24 @@ func (s *Common) NovaRequest(url string, params map[string]any, method string) (
 	return data, nil
 }
 
-func (s *Common) CloudRequest(url string, params map[string]any, method string) (res any, err error) {
+func (s *Common) CloudRequest(url string, params map[string]any, method string) (data map[string]interface{}, err error) {
 	req := http.Request{
 		Method: method,
 		URL:    url,
 		Body:   params,
 	}
-	res, err = request.Do(req, chg.Configure.CloudUrl)
-	return
+	res, err := request.Do(req, chg.Configure.CloudUrl)
+	if err != nil {
+		return nil, err
+	}
+	if res.Code != 200 {
+		return nil, errors.New(res.Msg)
+	}
+
+	bytes, _ := json.Marshal(res.Data)
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
