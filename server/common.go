376625414cf5f5241aba9ae3b7bfaca22dd8b8e1
yintao <gushui@139.com>
2024-03-28 16:08:58 +0800

    获取企业微信用户标签相关sdk测试

--- server/common.go
+++ server/common.go
@@ -69,3 +69,28 @@ func (s *Common) CloudRequest(url string, params map[string]any, method string) 
 	}
 	return data, nil
 }
+
+func (s *Common) CoreUserRequest(url string, params map[string]any, method string, header map[string]string) (data map[string]interface{}, err error) {
+	req := http.Request{
+		Method: method,
+		URL:    url,
+		Body:   params,
+	}
+	if header != nil {
+		req.Headers = header
+	}
+	res, err := request.Do(req, chg.Configure.CloudUrl)
+	if err != nil {
+		return nil, err
+	}
+	if res.Code != 200 {
+		return nil, errors.New(res.Msg)
+	}
+
+	bytes, _ := json.Marshal(res.Data)
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
