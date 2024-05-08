45a739a6d0068522477101d7d17de1b62365c2f9
yintao <gushui@139.com>
2024-05-08 15:10:56 +0800

    新增nexus通用请求接口

--- server/common.go
+++ server/common.go
@@ -26,6 +26,27 @@ func (s *Common) Request(url string, params map[string]any, method string) (res 
 	return
 }
 
+func (s *Common) NexusRequest(url string, params map[string]any, method string) (data map[string]interface{}, err error) {
+	req := http.Request{
+		Method: method,
+		URL:    url,
+		Body:   params,
+	}
+	res, err := request.Do(req, chg.Configure.NexusUrl)
+	if err != nil {
+		return nil, err
+	}
+	if res.Code != 200 {
+		return nil, errors.New(res.Msg)
+	}
+	bytes, _ := json.Marshal(res.Data)
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
 func (s *Common) NovaRequest(url string, params map[string]any, method string) (data map[string]interface{}, err error) {
 	req := http.Request{
 		Method: method,
